8563e5ca7dab275a9d42334e7d2af6a2a8523d59
Andrew Gunhee In <25ina@asij.ac.jp>
2021-02-26 13:16:55 +0900

    Initial Commit

---
+++ Shared/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Shared
+//
+//  Created by ANDREW GUNHEE IN on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
